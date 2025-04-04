c6f9cd928f1bada6a92a685a3464e6a51b58fca4
Luis <langelgirona@gmail.com>
2025-04-04 17:34:11 -0600

    Agregar métodos para agregar, eliminar y ver pacientes en VectorHeap

--- VectorHeap.java
+++ VectorHeap.java
@@ -9,16 +9,63 @@ public class VectorHeap<E extends Comparable<E>> extends PriorityQueue<E> {
         pacientes.add(null);
     }
 
-    protected static int parent(int i) {
+    protected static int padre(int i) {
         return i / 2;
     }
 
-    protected static int left(int i) {
+    protected static int izquierda(int i) {
         return 2 * i;
     }
 
-    protected static int right(int i) {
+    protected static int derecha(int i) {
         return 2 * i + 1;
     }
 
+    public String agregar(E nuevoPaciente) {
+        pacientes.add(nuevoPaciente);
+        int posicion = pacientes.size() - 1;
+        while (posicion > 1 && pacientes.get(posicion).compareTo(pacientes.get(padre(posicion))) < 0) {
+            E pacienteConMasPrioridad = pacientes.get(posicion);
+            pacientes.set(posicion, pacientes.get(padre(posicion)));
+            pacientes.set(padre(posicion), pacienteConMasPrioridad);
+            posicion = padre(posicion);
+        }
+        return "Paciente agregado: " + nuevoPaciente.toString();
+    }
+
+    public String eliminarPacienteConMasPrioridad() {
+        if (pacientes.size() <= 1) {
+            return null;
+        }
+        E pacienteEliminado = pacientes.get(1);
+        pacientes.set(1, pacientes.get(pacientes.size() - 1));
+        pacientes.remove(pacientes.size() - 1);
+        int posicion = 1;
+        while (izquierda(posicion) < pacientes.size()) {
+            int izquierdo = izquierda(posicion);
+            int derecho = derecha(posicion);
+            int hijoMenor = izquierdo;
+
+            if (derecho < pacientes.size() && pacientes.get(derecho).compareTo(pacientes.get(izquierdo)) < 0) {
+                hijoMenor = derecho;
+            }
+
+            if (pacientes.get(posicion).compareTo(pacientes.get(hijoMenor)) > 0) {
+                E pacienteConMenosPrioridad = pacientes.get(posicion);
+                pacientes.set(posicion, pacientes.get(hijoMenor));
+                pacientes.set(hijoMenor, pacienteConMenosPrioridad);
+                posicion = hijoMenor;
+            } else {
+                break;
+            }
+        }
+        return "Paciente eliminado: " + pacienteEliminado.toString();
+    }
+
+    public String verPacienteConMasPrioridad() {
+        if (pacientes.size() <= 1) {
+            return null;
+        }
+        return "Paciente con más prioridad: " + pacientes.get(1).toString();
+    }
 }
